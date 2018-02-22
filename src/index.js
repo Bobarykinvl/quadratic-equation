2acaf1129c5e40cf02cc7252437d9b3e8cc924a1
Bobarykinvl <bobarykinvl@gmail.com>
2018-02-22 23:36:56 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	var a,b,c,D,x1,x2,roots = [];
+ 	var str = equation.replace(/\s/g, '');
+	str = str.match(/[\d+-]\d+/g);
+	console.log(str);
+	a = str[0];
+	b = str[1];
+	c = str[2];		
+	D = Math.pow(b,2) - 4*a*c;
+	x1 = Math.round((-1)*b + Math.sqrt(D)) / (2*a);	
+	x2 = Math.round((-1)*b - Math.sqrt(D)) / (2*a);
+	roots.push(x1);	
+	roots.push(x2);	
+	roots.sort(function(a,b){
+		return a-b;
+	});
+	return roots;
+	
 }
